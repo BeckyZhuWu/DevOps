901c4b088c54590baa13be5e173f8b6be214891f
BeckyZhuWu <zhuwub@gmail.com>
2025-03-12 13:50:35 -0600

    se agrego Dockerfile y aplicacion REST

---
+++ SICEI/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Datos hardcoded
+const alumnos = [
+    { nombre: 'Becky Zhu', matricula: 'A15001597' },
+    { nombre: 'Alice francois', matricula: 'A12345678' },
+    { nombre: 'Hebert Negron', matricula: '15001887' },
+    { nombre: 'Pio pio', matricula: 'A11111111' }
+];
+
+const profesores = [
+    { nombre: 'Edwin ', numeroEmpleado: '001' },
+    { nombre: 'Victor', numeroEmpleado: '002' },
+    { nombre: 'Julio', numeroEmpleado: '003' },
+    { nombre: 'Eduardo', numeroEmpleado: '004' }
+];
+
+app.get('/alumnos', (req, res) => {
+    res.json(alumnos);
+});
+
+app.get('/profesores', (req, res) => {
+    res.json(profesores);
+});
+
+app.listen(port, () => {
+    console.log(`SICEI app listening at http://localhost:${port}`);
+});
